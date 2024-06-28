2ca79990f271e3a6ad53add49380df4e2d28a157
vas <vasantabhau@gmail.com>
2024-06-28 11:46:18 +0530

    bulid project

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import Header from "@/component/Header";
-import About from "./about/Page";
-import ContactUs from "./contact/page ";
 
 export default function Home() {
   return (
