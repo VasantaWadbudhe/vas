606ead5583e9905923d856bcc6b319cb99d6d61e
vas <vasantabhau@gmail.com>
2024-06-28 11:25:39 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "@/component/Header";
+import About from "./about/Page";
+import ContactUs from "./contact/page ";
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
